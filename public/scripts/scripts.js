907a1a5f1ed39a8eeaa034eb1cb9b5ea8673aab1
CCLiff <CLCliff30@gmail.com>
2015-01-26 17:42:31 -0500

    adds more styling to the dropdown headers. submitting current version for feedback

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -7,9 +7,12 @@ $(document).ready(function(){
 
   $('#user-nav a').on('click', function(e){
     e.preventDefault();
+    $('#user-nav .active').removeClass('active');
+    $(this).addClass('active');
     var menu = $(this).attr('href');
     if($('#'+ menu + '-dropdown .dropdown-wrapper').hasClass('dropdown-active')){
       $('.dropdown-wrapper').removeClass('dropdown-active');
+      $('#user-nav .active').removeClass('active');
     }else if($('.dropdown-wrapper').hasClass('dropdown-active')){
       $('.dropdown-wrapper').removeClass('dropdown-active');
       // This timer must be equal to the CSS transition timer
